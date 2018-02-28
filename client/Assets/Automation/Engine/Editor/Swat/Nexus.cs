2f250306374e4d422eaf3dd96f9a0aa46d73a553
Cham <36428049+disruptorCham@users.noreply.github.com>
2018-02-28 17:44:47 -0500

    license

--- client/Assets/Automation/Engine/Editor/Swat/Nexus.cs
+++ client/Assets/Automation/Engine/Editor/Swat/Nexus.cs
@@ -1,4 +1,23 @@
-﻿//TODO: Remove pragma; Update `new EditorWindow` instances to get rid of "must instantiate with ScriptableObject" errors.
+/* 
+   This file is part of Trilleon.  Trilleon is a client automation framework.
+  
+   Copyright (C) 2017 Disruptor Beam
+  
+   Trilleon is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//TODO: Remove pragma; Update `new EditorWindow` instances to get rid of "must instantiate with ScriptableObject" errors.
 /* This warning is not a problem programmatically because the ScriptableObject.CreateInstance<>() call is indeed being used when
  * the actual window is being shown. The "new EditorWindow" calls merely instantiate the class allowing the "Show()" method to be called.
 */
