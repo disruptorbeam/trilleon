8271f01c97d589e161ba2800e2d8dc04f987cedf
Cham <36428049+disruptorCham@users.noreply.github.com>
2018-02-28 16:05:04 -0500

    add wrappers

---
+++ client/Assets/Automation/Engine/Wrappers/UnityConsoleBroker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace TrilleonAutomation {
+
+   public static class UnityConsoleBroker  {
+
+      /// <summary>
+      /// Run cheat command which has a behavior dictated by a test case or test initialize/tear down.
+      /// </summary>
+      /// <param name="command">Command.</param>
+      /// <param name="argsString">Arguments string with parameters seperated by spaces.</param>
+      public static void SendCommand(string command, string argsString = "") {
+         
+         if(!string.IsNullOrEmpty(argsString)) {
+            
+            string[] args = argsString.Split(' ');
+            Wenzil.Console.Console.ExecuteCommand(command, args);
+
+         } else {
+            
+            Wenzil.Console.Console.ExecuteCommand(command);
+
+         }
+      }
+
+   }
+
+}
